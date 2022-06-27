2db566f3fd7602cf93d404e7cdae321873dff929
Official-Rup <107984412+Official-Rup@users.noreply.github.com>
2022-06-27 19:59:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['918617845264']
+global.premium = ['918617845264']
+global.ownernomer = '918617845264'
+global.ownername = '👅😎OFFICIAL-RUP❤👀'
+global.botname = 'RUP❤AHANA-V3'
+global.footer = '©RUP-OFFICIAL.INC.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'RUP❤AHANA-V3'
+global.author = '👅😎OFFICIAL-RUP❤👀'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
@@ -48,7 +48,7 @@ global.mess = {
 }
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 120,
     monayawal: 1000
 }
    global.rpg = {
